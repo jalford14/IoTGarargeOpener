dd70e3fe9114bcbdbdef7ebbe4285edcc33f9f36
James Alford <jalford14@liberty.edu>
2017-03-27 12:04:58 -0400

    Update main.php
    Got rid of access token and device id information

--- garage_door/main.php
+++ garage_door/main.php
@@ -1,5 +1,5 @@
 <?php
-	$url = "https://api.particle.io/v1/devices/30002f000c51343334363138/led?access_token=b25927445524a0d2b01e1bbd9896ad4d410b723b";
+	$url = "https://api.particle.io/v1/devices/DEVICEID/led?access_token=ACCESSTOKEN";
 
     $ch = curl_init($url);
     curl_setopt( $ch, CURLOPT_POST, 1);
@@ -7,7 +7,7 @@
     curl_setopt( $ch, CURLOPT_HEADER, 0);
     curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt( $ch, CURLOPT_POSTFIELDS, array('arg' => 'on'));
-    //curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Authorization: b25927445524a0d2b01e1bbd9896ad4d410b723b');
+    //curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Authorization: ACCESSTOKEN');
     
     $response = curl_exec( $ch );
 ?>
